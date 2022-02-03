e7e1988a4d01f0c89063ee831fb48187e15e41d6
nicmorais <nmorais.st@gmail.com>
2022-02-03 16:27:51 -0300

    Removes databases

--- config/test.exs
+++ config/test.exs
@@ -6,7 +6,7 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :cross_comerce_etl, CrossComerceEtl.Repo,
-  database: "cross_comerce_etl_test#{System.get_env("MIX_TEST_PARTITION")}",
+  database: "cross_comerce_etl_test#{System.get_env("MIX_TEST_PARTITION")}.sqlite3",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
